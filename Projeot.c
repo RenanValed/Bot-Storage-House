221f21ae9b7952e3feca9df05a973089a9bd1ca3
RenanValed <77640663+RenanValed@users.noreply.github.com>
2021-01-20 15:32:08 -0300

    Create Projeot.c

---
+++ Projeot.c
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///                Developed by  @ooo.renan - Jan, 2021                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+
+}
+void menuSobre(void) {
+   
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Sistema de Gestão Escolar                      ///\n");
+    printf("///                Developed by  @ooo.renan - Jan, 2021                     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                  = = = = =  Bot Storage House  = = = = =                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///    Este bot tem como função monitorar o armazenamento de alimentos e    ///\n");
+    printf("///  Produtos, afim de promover economia e melhorar os hábitos alimentícios ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    
+}
