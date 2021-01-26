0b63f90647e84feaa889ce4aade5338c51e1a746
Renan Vale <renanvale_d@hotmail.com>
2021-01-26 19:43:24 +0000

    Menu Bot criado

--- Projeto.c
+++ Projeto.c
@@ -1,15 +1,20 @@
 //
-//             Universidade Federal do Rio Grande do Norte                 ~~~
-//                 Centro de Ensino Superior do Seridó                     ~~~
-//~~~               Departamento de Computação e Tecnologia                   ~~~
-//~~~                  Disciplina DCT1106 -- Programação                      ~~~
-//~~~                  Projeto Sistema de Gestão Escolar                      ~~~
-//~~~                Developed by  @ooo.renan - Jan, 2021                    ~~~
+//             Universidade Federal do Rio Grande do Norte                 ===
+//                 Centro de Ensino Superior do Seridó                     ===
+//===               Departamento de Computação e Tecnologia                   ===
+//===                  Disciplina DCT1106 -- Programação                      ===
+//===                  Projeto Sistema de Gestão Escolar                      ===
+//===                Developed by  @ooo.renan - Jan, 2021                    ===
 //
 
 #include <stdio.h>
-
+#include <stdlib.h>
+//Interfaces
 void menuSobre(void);
+void menubot(void);
+void menudono(void);
+void menumorador(void);
+
 
 int main(void) {
     menuSobre();
@@ -17,32 +22,49 @@ int main(void) {
 
 }
 void menuSobre(void) {
-   printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("~~~                                                                         ~~~\n");
-    printf("~~~ CCCCC   CCCCC  CC   CC        C       C   C  CC    C  CCC    CCCCC      ~~~\n");
-    printf("~~~ C   C   C      C C C C         C     C    C  C C   C  C  C   C   C      ~~~\n");
-    printf("~~~ CCCC    CCC    C  C  C  CCCC    C   C     C  C  C  C  C   C  C   C      ~~~\n");
-    printf("~~~ C   C   C      C     C           C C      C  C   C C  C  C   C   C      ~~~\n");
-    printf("~~~ CCCC    CCCCC  C     C            C       C  C    CC  CCC    CCCCC      ~~~\n");
-    printf("~~~                                                                         ~~~\n");
-    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+   printf("================================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("=== CCCCC   CCCCC  CC   CC        C       C   C  CC    C  CCC    CCCCC      ===\n");
+    printf("=== C   C   C      C C C C         C     C    C  C C   C  C  C   C   C      ===\n");
+    printf("=== CCCC    CCC    C  C  C  CCCC    C   C     C  C  C  C  C   C  C   C      ===\n");
+    printf("=== C   C   C      C     C           C C      C  C   C C  C  C   C   C      ===\n");
+    printf("=== CCCC    CCCCC  C     C            C       C  C    CC  CCC    CCCCC      ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===             Universidade Federal do Rio Grande do Norte                 ===\n");
+    printf("===                 Centro de Ensino Superior do Seridó                     ===\n");
+    printf("===               Departamento de Computação e Tecnologia                   ===\n");
+    printf("===                  Disciplina DCT1106 -- Programação                      ===\n");
+    printf("===                  Projeto Sistema de Gestão Escolar                      ===\n");
+    printf("===                Developed by  @ooo.renan - Jan, 2021                     ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
 
-    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("~~~                                                                         ~~~\n");
-    printf("~~~             Universidade Federal do Rio Grande do Norte                 ~~~\n");
-    printf("~~~                 Centro de Ensino Superior do Seridó                     ~~~\n");
-    printf("~~~               Departamento de Computação e Tecnologia                   ~~~\n");
-    printf("~~~                  Disciplina DCT1106 -- Programação                      ~~~\n");
-    printf("~~~                  Projeto Sistema de Gestão Escolar                      ~~~\n");
-    printf("~~~                Developed by  @ooo.renan - Jan, 2021                     ~~~\n");
-    printf("~~~                                                                         ~~~\n");
-    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-
-    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("~~~                  = = = = =  Bot Storage House  = = = = =                ~~~\n");
-    printf("~~~                                                                         ~~~\n");
-    printf("~~~    Este bot tem como função monitorar o armazenamento de alimentos e    ~~~\n");
-    printf("~~~  Produtos, afim de promover economia e melhorar os hábitos alimentícios ~~~\n");
-    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("===============================================================================\n");
+    printf("===                  = = = = =  Bot Storage House  = = = = =                ===\n");
+    printf("===                                                                         ===\n");
+    printf("===    Este bot tem como função monitorar o armazenamento de alimentos e    ===\n");
+    printf("===  Produtos, afim de promover economia e melhorar os hábitos alimentícios ===\n");
+    printf("===============================================================================\n");
     
+}
+
+void menubot(void){
+  system("cls");
+    printf("\t\t========================================================");
+    printf("\t\t==               Menu Bot Control                     ==");
+    printf("\t\t========================================================");
+    printf("\t\t==                                                    ==");
+    printf("\t\t==    1- Configurar                                   ==");
+    printf("\t\t==    2- Atualizar Dados                              ==");
+    printf("\t\t==    3- Relatório                                    ==");
+    printf("\t\t==    4- Exibir Informações Atuais                    ==");
+    printf("\t\t==    5- Configurar Notificações                      ==");
+    printf("\t\t==    6- Deletar Dados                                ==");
+    printf("\t\t========================================================");
+  printf("\t\t\t→ click <ENTER> for next...\n");
+  getchar();
+
 }
